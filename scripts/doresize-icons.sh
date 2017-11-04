93b80dbc5f0357e45c4b8581e837c6a378a40ab3
Kyle Graehl <kyle@graehlarts.com>
2017-11-04 18:54:38 +0100

    add package script

---
+++ scripts/doresize-icons.sh
@@ -0,0 +1,1 @@
+for sz in 512 128 96 64 48 38 32 19 16; do convert -resize $szx$sz app/icons/icon-orig.png app/icons/icon-$sz.png; done
---
+++ scripts/package.sh
@@ -0,0 +1,2 @@
+rm package.zip
+zip -r package.zip manifest.json *.html js/*.js icons/icon-*.png dist/*.js css/*.css
